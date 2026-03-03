6f9abf272de15e8f65855807602b9702d52a44ba
SMH01 <smali.pg@outlook.com>
2026-03-03 21:13:39 +0300

    chore: add Compose Animation dependency
    * chore: add `androidx.compose.animation:animation` to `app/build.gradle.kts` dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -167,6 +167,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.navigation:navigation-compose:2.8.5")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("androidx.compose.animation:animation")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
