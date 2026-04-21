8e600b9c4f61b7401831d9eb89bc9d38797bf9dc
SMH01 <smali.pg@outlook.com>
2026-04-21 15:11:12 +0300

    feat: update Warp API endpoint and add request headers
    * feat: update the default Warp API URL to `https://warp-vercel-murex.vercel.app/api/warp-data`
    * feat: add browser-like HTTP headers to `getWarpData`, including User-Agent, Referer, and CORS metadata, to improve request compatibility

--- app/src/main/java/ru/protonmod/next/data/network/WarpApi.kt
+++ app/src/main/java/ru/protonmod/next/data/network/WarpApi.kt
@@ -5,9 +5,20 @@
 package ru.protonmod.next.data.network
 
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.Url
 
 interface WarpApi {
+    @Headers(
+        "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.1.7585.101 Safari/537.36",
+        "Accept: */*",
+        "Accept-Language: ru",
+        "Referer: https://warp-generator.github.io/",
+        "Origin: https://warp-generator.github.io",
+        "Sec-Fetch-Dest: empty",
+        "Sec-Fetch-Mode: cors",
+        "Sec-Fetch-Site: cross-site"
+    )
     @GET
-    suspend fun getWarpData(@Url url: String = "https://warp-generator-config.vercel.app/api/warp-data"): WarpResponse
+    suspend fun getWarpData(@Url url: String = "https://warp-vercel-murex.vercel.app/api/warp-data"): WarpResponse
 }
